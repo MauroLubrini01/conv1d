e35e9f3bced678b802e5a48932f9e9844d7a88db
MauroLubrini01 <s328825@studenti.polito.it>
2025-01-01 17:56:53 +0100

    Update multiplier.sv

--- hw/hw_accel-comp/multiplier.sv
+++ hw/hw_accel-comp/multiplier.sv
@@ -1,9 +1,9 @@
 module multiplier #(
   parameter N = 8
 ) (
-  input  [    N-1:0] a,
-  input  [    N-1:0] b,
-  output [(N*2)-1:0] p
+  input signed [    N-1:0] a,
+  input signed [    N-1:0] b,
+  output signed [(N*2)-1:0] p
 );
 
   assign p = a * b;
